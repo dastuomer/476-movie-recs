3f5a044c030e5ff5712eba6c78efaf84903ac9fa
Alex Kuntz <alexanderaak@hotmail.com>
2024-02-24 03:09:48 -0600

    Made functions for different pages

---
+++ film-finder/src/app/index-page/index.jsx
@@ -0,0 +1,7 @@
+export default function Index() {
+    return (
+        <main>
+            <h1>This is Index Page!</h1>
+        </main>
+    )
+}
---
+++ film-finder/src/app/login-page/login.jsx
@@ -0,0 +1,7 @@
+export default function Login() {
+    return (
+        <main>
+            <h1>This is Login Page!</h1>
+        </main>
+    )
+}
