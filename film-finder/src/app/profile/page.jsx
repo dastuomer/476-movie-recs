d5a7305e626cef0eab0c36ffd0740b48a758acb5
Alex Kuntz <alexanderaak@hotmail.com>
2024-03-08 22:48:43 -0600

    Grid Structure

--- film-finder/src/app/profile/page.jsx
+++ film-finder/src/app/profile/page.jsx
@@ -35,21 +35,27 @@ export default function Page() {
     return (
         <ChakraProvider>
             <Center>
-                <Box maxW='lg' borderWidth='3px' borderRadius='lg' overflow='hidden' backgroundColor={'grey'}>
-                    <Circle margin='5px'>
-                        <Avatar size='lg' src = 'https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*'>
-                            <AvatarBadge boxSize="1.25em" bg="green.800" />
-                        </Avatar>   
-                    </Circle>      
-                    <Heading fontSize="4xl" align={"center"}>
-                        User Name
-                    </Heading>
-                    <Box borderWidth='3px' borderRadius='lg' margin='5px' borderColor={'black'}>
-                        <Text fontSize="xl" align={"center"} margin='3px'> 
-                            User Bio Goes Here.
-                        </Text>
-                    </Box>
-                </Box>
+                <div className="container-fluid">
+                    <div className="row justify-content-center">
+                      <div className="col-3 bg-danger">
+                        <Box maxW='lg' borderWidth='3px' borderRadius='lg' overflow='hidden' backgroundColor={'grey'}>
+                            <Circle margin='5px'>
+                                <Avatar size='lg' src = 'https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*'>
+                                    <AvatarBadge boxSize="1.25em" bg="green.800" />
+                                </Avatar>   
+                            </Circle>      
+                            <Heading fontSize="4xl" align={"center"}>
+                                User Name
+                            </Heading>
+                            <Box borderWidth='3px' borderRadius='lg' margin='5px' borderColor={'black'}>
+                                <Text fontSize="xl" align={"center"} margin='3px'> 
+                                    User Bio Goes Here.
+                                </Text>
+                            </Box>
+                        </Box>
+                      </div>
+                    </div>
+                </div>
             </Center>
         </ChakraProvider>
         
