e54a5f6f7c3db20dc5c04e53dd42a94a141be429
Alex Kuntz <alexanderaak@hotmail.com>
2024-03-11 00:01:33 -0600

    Blender and View Friend Review Page Mock

---
+++ film-finder/src/app/components/movie-picture.jsx
@@ -0,0 +1,8 @@
+import Image from "next/image";
+export default function TheReturnOfTheKing() {
+  return (
+    <div>
+      <Image src="/Return of Kiing.jpg" width={200} height={200} alt="Movie Pic" />
+    </div>
+  );
+}
