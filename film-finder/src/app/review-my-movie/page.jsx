ed7cb6df4bcbb557ac46cae3ec790ded3a483c1b
MaxxM77 <162059169+MaxxM77@users.noreply.github.com>
2024-03-13 17:30:46 -0600

    Movie Reviews
    Added button to go to movie reviews page.
    added layout for move reviews page

--- film-finder/src/app/review-my-movie/page.jsx
+++ film-finder/src/app/review-my-movie/page.jsx
@@ -1,7 +1,94 @@
+import * as React from "react";
+import Logo from "../components/logo.js";
+import {
+  Drawer,
+  DrawerBody,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  Divider,
+  Button,
+  IconButton,
+  Input,
+  useDisclosure,
+  Avatar,
+  AvatarBadge,
+  HStack,
+  Stack,
+  Text,
+  Box,
+  Center,
+  Circle,
+  Heading,
+  Link,
+  Image,
+  Grid,
+  Textarea,
+  WrapItem,
+  Container, 
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  AccordionIcon,
+  Tab,
+  TabList,
+  TabPanels,
+  Tabs,
+  TabPanel,
+  extendTheme,
+  ColorModeScript,
+  Flex
+} from "@chakra-ui/react";
+import { ChakraProvider } from '@chakra-ui/react'
+//import { StarIcon } from '@chakra-ui/icons'
+
 export default function Page() {
-    return (
-        <main>
-            <h1>This is review my movie Page!</h1>
-        </main>
-    )
+  return (
+    <ChakraProvider>
+      <Box backgroundColor="#2D3748">
+        <div className="container-fluid">
+          <div className="row justify-content-center">
+            <div className="col-6">  
+              <Box w='100%' minH='1400px' borderWidth='5px' boxShadow='dark-lg' borderColor='#171923' borderRadius='lg' backgroundColor='#A0AEC0'>
+                <Logo/>
+                <Center>
+                    <Text fontSize="5xl">Movie Reviews</Text>
+                </Center>
+                <Image boxSize='400px' w='100%' borderBottom='5px' borderColor='black' objectFit='cover' filter='auto' src='https://m.media-amazon.com/images/S/pv-target-images/a01a95f5ccf69d4946bc47259a10f7fa9b5368963870bfe37c3c2f314e50ee2d._SX1080_FMjpg_.jpg'></Image>
+                <div>
+                    <Text fontSize='3xl' margin="10px">V For Vendetta</Text>
+                    *Insert stars here + small description*
+                </div>
+                <br/>
+                <Center>
+                    <Box w="95%" borderWidth="3px" borderRadius="lg" borderColor="grey" marginBottom="15px">
+                        <Text margin="10px" fontSize="md">Muck's Review:  *stars*</Text>
+                        <Text margin="10px" fontSize="md">Zander watch the movie it's a good one actually Zander watch the movie it's a good one actually Zander watch the movie it's a good one actually Zander watch the movie it's a good one actually Zander watch the movie it's a good one actually Zander watch the movie it's a good one actually Zander watch the movie it's a good one actually Zander watch the movie it's a good one actually Zander watch the movie it's a good one actually Zander watch the movie it's a good one actually Zander watch the movie it's a good one actually Zander watch the movie it's a good one actually Zander watch the movie it's a good one actually </Text>
+                    </Box>
+                </Center>
+                <Center>
+                    <Box w="95%" borderWidth="3px" borderRadius="lg" borderColor="grey" marginBottom="15px">
+                        <Text margin="10px" fontSize="md">Muck's Review:  *stars*</Text>
+                        <Text margin="10px" fontSize="md">Zander watch the movie it's a good one actually Zander watch the movie it's a good one actually Zander watch the movie it's a good one actually Zander watch the movie it's a good one actually Zander watch the movie it's a good one actually Zander watch the movie it's a good one actually Zander watch the movie it's a good one actually Zander watch the movie it's a good one actually Zander watch the movie it's a good one actually Zander watch the movie it's a good one actually Zander watch the movie it's a good one actually Zander watch the movie it's a good one actually Zander watch the movie it's a good one actually </Text>
+                    </Box>
+                </Center>
+                <Center>
+                    <Box w="95%" borderWidth="3px" borderRadius="lg" borderColor="grey" marginBottom="15px">
+                        <Text margin="10px" fontSize="md">Muck's Review:  *stars*</Text>
+                        <Text margin="10px" fontSize="md">Zander watch the movie it's a good one actually Zander watch the movie it's a good one actually Zander watch the movie it's a good one actually Zander watch the movie it's a good one actually Zander watch the movie it's a good one actually Zander watch the movie it's a good one actually Zander watch the movie it's a good one actually Zander watch the movie it's a good one actually Zander watch the movie it's a good one actually Zander watch the movie it's a good one actually Zander watch the movie it's a good one actually Zander watch the movie it's a good one actually Zander watch the movie it's a good one actually </Text>
+                    </Box>
+                </Center>
+                <Center>
+                    <Button colorScheme='blue' margin='5px' > <Link href="view-my-movies">View Movies</Link></Button>
+                </Center>
+              </Box>
+            </div>
+          </div>
+        </div>
+      </Box>
+    </ChakraProvider>   
+  )
 }
--- film-finder/src/app/view-my-movies/page.jsx
+++ film-finder/src/app/view-my-movies/page.jsx
@@ -69,32 +69,38 @@ export default function Page() {
                   <TabPanels>
                     <TabPanel>
                       <div>
-                        <Text colorScheme='blue' fontSize="2xl" >My Movies</Text>
+                        <Text colorScheme='blue' fontSize="3xl" >My Movies</Text>
                       </div>
                       <Grid templateColumns='repeat(4, 1fr)' gap='10px' margin='5px'>
                         <Image width='200px' height='250px' objectFit='cover' src="https://miro.medium.com/v2/resize:fit:720/format:webp/1*Etx-JkqJ5fGz9DnwLVlzNA.jpeg"></Image>
                         <div>
                           <Text>V For Vendetta</Text>
+                          <Button colorScheme='blue' margin='5px' pos='absolute'> <Link href="review-my-movie">Reviews</Link></Button>
                         </div>
                         <Image width='200px' height='250px' objectFit='cover' src="https://musicimage.xboxlive.com/catalog/video.movie.8D6KGWZL59DS/image?locale=en-ie&mode=crop&purposes=BoxArt&q=90&h=300&w=200&format=jpg"></Image>
                         <div>
                           <Text>The Green Mile</Text>
+                          <Button colorScheme='blue' margin='5px' pos='absolute'> <Link href="review-my-movie">Reviews</Link></Button>
                         </div>
                         <Image width='200px' height='250px' objectFit='cover' src="https://i.ebayimg.com/images/g/uHoAAOSw5yxjhNAI/s-l500.jpg"></Image>
                         <div>
                           <Text>Contact</Text>
+                          <Button colorScheme='blue' margin='5px' pos='absolute'> <Link href="review-my-movie">Reviews</Link></Button>
                         </div>
                         <Image width='200px' height='250px' objectFit='cover' src="https://m.media-amazon.com/images/M/MV5BNGYyZGM5MGMtYTY2Ni00M2Y1LWIzNjQtYWUzM2VlNGVhMDNhXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_FMjpg_UX1000_.jpg"></Image>
                         <div>
                           <Text>Pirates of The Caribbean: The Curse of The Black Pearl</Text>
+                          <Button colorScheme='blue' margin='5px' pos='absolute'> <Link href="review-my-movie">Reviews</Link></Button>
                         </div>
                         <Image width='200px' height='250px' objectFit='cover' src="https://images.squarespace-cdn.com/content/v1/5435f72ae4b09dbd92eb43ad/1585767951967-KP1BCETVEEQJ6WD7QOLQ/Hobbit+movie.jpg"></Image>
                         <div>
                           <Text>The Hobbit: An Unexpected Journey</Text>
+                          <Button colorScheme='blue' margin='5px' pos='absolute'> <Link href="review-my-movie">Reviews</Link></Button>
                         </div>
                         <Image width='200px' height='250px' objectFit='cover' src="https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/p629_p_v8_af.jpg"></Image>
                         <div>
                           <Text>Monty Python and The Holy Grail</Text>
+                          <Button colorScheme='blue' margin='5px' pos='absolute'> <Link href="review-my-movie">Reviews</Link></Button>
                         </div>
                       </Grid>
                     </TabPanel>
