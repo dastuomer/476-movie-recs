a256936591bc408cbd2ae04bcaf4731c8f8ef6de
MaxxM77 <162059169+MaxxM77@users.noreply.github.com>
2024-03-20 15:03:05 -0600

    reviews page
    -changed layout to use proper movie poster

--- film-finder/src/app/review-my-movie/page.jsx
+++ film-finder/src/app/review-my-movie/page.jsx
@@ -29,7 +29,7 @@ import {
   Grid,
   Textarea,
   WrapItem,
-  Container, 
+  Container,
   Accordion,
   AccordionItem,
   AccordionButton,
@@ -45,7 +45,6 @@ import {
   Flex
 } from "@chakra-ui/react";
 import { ChakraProvider } from '@chakra-ui/react'
-//import { StarIcon } from '@chakra-ui/icons'
 
 export default function Page() {
   return (
@@ -56,64 +55,58 @@ export default function Page() {
           <div className="row justify-content-center">
             <Flex>
               <div className="col-1">
-                <a href='/'><Logo/></a>
+                <a href='/'><Logo /></a>
               </div>
-            <div className="col-10">
-              <Box w='100%' minH='1400px' borderWidth='5px' boxShadow='dark-lg' borderColor='#171923' borderRadius='lg' backgroundColor='#A0AEC0'>
-                <Center>
+              <div className="col-10">
+                <Box w='100%' minH='1400px' borderWidth='5px' boxShadow='dark-lg' borderColor='#171923' borderRadius='lg' backgroundColor='#A0AEC0'>
+                  <Center>
                     <Text fontSize="5xl" as="b">Movie Reviews</Text>
-                </Center>
-                <Image boxSize='600px' w='100%' borderBottom='5px' borderColor='black' objectFit='cover' filter='auto' src='https://m.media-amazon.com/images/S/pv-target-images/a01a95f5ccf69d4946bc47259a10f7fa9b5368963870bfe37c3c2f314e50ee2d._SX1080_FMjpg_.jpg'></Image>
-                <div>
-                    <Text fontSize='4xl' margin="15px" marginLeft="30px" as="u"> V For Vendetta </Text>
-
-                </div>
-                <br/><br/>
-                <Flex>
-                  <Text marginLeft="175px" marginRight="10px" fontSize={20}>Rating:</Text>
-                  <StarRating />
-                </Flex>
-                <Center>
-                    <Textarea w="75%" outlineColor="black" minHeight="200px" placeholder="Write your review!"></Textarea>
-
-
-
-                </Center>
-                <Button colorScheme='blue' margin='15px' marginLeft="1070"> <Link href="view-my-movies">Submit Review</Link></Button>
-                <br/><br/>
-                <Divider/>
-                <Center>
-                <Grid templateColumns='repeat(2, 1fr)' gap='10px' margin='35px'>
-                    <Box w="95%" borderWidth="3px" borderRadius="lg" borderColor="grey" marginBottom="15px">
+                  </Center>
+                  <Divider />
+                  <Flex margin="50px">
+                    <Image width='400px' height='500px' objectFit='cover' margin="20px" src="https://miro.medium.com/v2/resize:fit:720/format:webp/1*Etx-JkqJ5fGz9DnwLVlzNA.jpeg"></Image>
+                    <Box w="55%">
+                      <Text fontSize='4xl' margin="15px" marginLeft="30px" as="u"> V For Vendetta </Text>
+                      <Text margin="5px" marginLeft="30px"> - Action, Horror, Comedy</Text>
+                      <Flex marginTop="40px">
+                        <Text marginLeft="45px" marginRight="10px" fontSize={20}>Rating:</Text>
+                        <StarRating />
+                      </Flex>
+                      <Textarea w="100%" outlineColor="black" minHeight="200px" marginLeft="45px" placeholder="Write your review!"></Textarea>
+                      <Button colorScheme='blue' margin='30px' marginLeft="55px"> <Link href="view-my-movies">Submit Review</Link></Button>
+                    </Box>
+                  </Flex>
+                  <Divider />
+                  <Center>
+                    <Grid templateColumns='repeat(2, 1fr)' gap='10px' margin='35px'>
+                      <Box w="95%" borderWidth="3px" borderRadius="lg" borderColor="grey" marginBottom="15px">
                         <Flex>
                           <Text margin="10px" fontSize="md">Muck's Review: </Text>
                           <StarRating />
                         </Flex>
                         <Text margin="10px" fontSize="md">Zander watch the movie it's a good one actually Zander watch the movie it's a good one actually Zander watch the movie it's a good one actually Zander watch the movie it's a good one actually Zander watch the movie it's a good one actually Zander watch the movie it's a good one actually Zander watch the movie it's a good one actually Zander watch the movie it's a good one actually Zander watch the movie it's a good one actually Zander watch the movie it's a good one actually Zander watch the movie it's a good one actually Zander watch the movie it's a good one actually Zander watch the movie it's a good one actually </Text>
-                    </Box>
-
-
-                    <Box w="95%" borderWidth="3px" borderRadius="lg" borderColor="grey" marginBottom="15px">
+                      </Box>
+                      <Box w="95%" borderWidth="3px" borderRadius="lg" borderColor="grey" marginBottom="15px">
                         <Flex>
                           <Text margin="10px" fontSize="md">Muck's Review: </Text>
                           <StarRating />
                         </Flex>
                         <Text margin="10px" fontSize="md">Zander watch the movie it's a good one actually Zander watch the movie it's a good one actually Zander watch the movie it's a good one actually Zander watch the movie it's a good one actually Zander watch the movie it's a good one actually Zander watch the movie it's a good one actually Zander watch the movie it's a good one actually Zander watch the movie it's a good one actually Zander watch the movie it's a good one actually Zander watch the movie it's a good one actually Zander watch the movie it's a good one actually Zander watch the movie it's a good one actually Zander watch the movie it's a good one actually </Text>
-                    </Box>
-                  </Grid>
-                </Center>
-                <Center>
+                      </Box>
+                    </Grid>
+                  </Center>
+                  <Center>
                     <Button colorScheme='blue' margin='15px' > <Link href="view-my-movies">View Movies</Link></Button>
-                </Center>
-              </Box>
-            </div>
+                  </Center>
+                </Box>
+              </div>
               <div className="col-1">
-                  <NavDrawer/>
+                <NavDrawer />
               </div>
             </Flex>
           </div>
         </div>
       </Box>
-    </ChakraProvider>   
+    </ChakraProvider>
   )
 }
