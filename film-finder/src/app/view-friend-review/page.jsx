7abe557487ce36a4456efca436a5fa338475e2fc
MaxxM77 <162059169+MaxxM77@users.noreply.github.com>
2024-03-21 18:44:04 -0600

    Dynamic data
    -Removed data placeholders
    -Added pseudo-code for dynamic data display once database is connected

--- film-finder/src/app/view-friend-review/page.jsx
+++ film-finder/src/app/view-friend-review/page.jsx
@@ -5,47 +5,15 @@ import NavDrawer from "@/app/components/Drawer";
 import { ChakraProvider } from "@chakra-ui/react";
 import StarRating from "../components/stars.js";
 import {
-    Drawer,
-    DrawerBody,
-    DrawerFooter,
-    DrawerHeader,
-    DrawerOverlay,
-    DrawerContent,
-    DrawerCloseButton,
     Divider,
-    Button,
-    IconButton,
     Input,
-    useDisclosure,
-    Avatar,
-    AvatarBadge,
-    HStack,
-    Stack,
     Text,
     Box,
-    Center,
-    Circle,
-    Heading,
-    Link,
-    Image,
-    Grid,
-    Textarea,
-    WrapItem,
-    Container,
-    Accordion,
-    AccordionItem,
-    AccordionButton,
-    AccordionPanel,
-    AccordionIcon,
-    Tab,
-    TabList,
-    TabPanels,
-    Tabs,
-    TabPanel,
-    extendTheme,
-    ColorModeScript,
-    Flex
+    Flex,
+    Image
 } from "@chakra-ui/react";
+
+{/*Obtain Selected User's Username, and Review list*/}
 
 export default function Page() {
     return (
@@ -62,30 +30,33 @@ export default function Page() {
                                 <Box w='100%' minH='1400px' borderWidth='5px' boxShadow='dark-lg' borderColor='#171923' borderRadius='lg' backgroundColor='#A0AEC0'>
                                     <div className="d-flex justify-content-center mt-4">
                                         <div>
-                                            <h1>Xander's Movie Reviews</h1>
+                                            <h1>User's Movie Reviews</h1>{/*Selected User's Username*/}
                                         </div>
                                     </div>
                                     <div className="row justify-content-center">
                                         <div className="col-9">
-                                            <Input placeholder='Search Movies' />
+                                            <Input placeholder='Search Movies' />{/*Filter's selected User's review list for movies*/}
                                         </div>
                                     </div>
                                     <br /><Divider /><br />
                                     <div className="row justify-content-center">
                                         <div className="col-11">
                                             <Box className="container-fluid" bg='#4A5568' w='100%' p={20} color='white' marginTop={3} borderRadius='20'>
+{/*Loop for each Review Selected user has made*/}
+{/*Obtain each movie's Poster, Title, Review and Selected User's rating*/}
                                                 <Flex gap="20px">
                                                     <div className="col-2">
-                                                        <TheReturnOfTheKing className="align-left" />
-                                                        <StarRating />
+                                                        <Image className="align-left" width={200} height={200} src=""></Image>{/*Movie's Poster*/}
+                                                        <StarRating />{/*User's rating of the movies*/}
                                                     </div>
                                                     <div>
-                                                        <Text fontSize="25">The Lord of The Rings: The Return of The King</Text>
+                                                        <Text fontSize="25">Movie Title</Text>{/*Movie title*/}
                                                         <Box borderColor="black" borderWidth="2px" borderRadius="5px" height="250px" width="1000px">
-                                                            <Text fontSize="16" margin="10px">This movie was really good. I liked the actors, and the action is cool. Wish I could have seen butt-ass naked Gandalf though :/</Text>
+                                                            <Text fontSize="16" margin="10px">Selected User's Review of the movie</Text>{/*Selected user's review text*/}
                                                         </Box>
                                                     </div>
                                                 </Flex><br/>
+{/*End Loop*/}
                                             </Box>
                                         </div>
                                     </div>
--- film-finder/src/app/view-my-movies/page.jsx
+++ film-finder/src/app/view-my-movies/page.jsx
@@ -3,48 +3,25 @@ import Logo from "../components/logo.js";
 import StarRating from "../components/stars.js";
 import NavDrawer from "@/app/components/Drawer";
 import {
-  Drawer,
-  DrawerBody,
-  DrawerFooter,
-  DrawerHeader,
-  DrawerOverlay,
-  DrawerContent,
-  DrawerCloseButton,
   Divider,
   Button,
-  IconButton,
   Input,
-  useDisclosure,
-  Avatar,
-  AvatarBadge,
-  HStack,
-  Stack,
   Text,
   Box,
   Center,
-  Circle,
-  Heading,
   Link,
   Image,
   Grid,
-  Textarea,
-  WrapItem,
-  Container, 
-  Accordion,
-  AccordionItem,
-  AccordionButton,
-  AccordionPanel,
-  AccordionIcon,
   Tab,
   TabList,
   TabPanels,
   Tabs,
   TabPanel,
-  extendTheme,
-  ColorModeScript,
   Flex
 } from "@chakra-ui/react";
 import { ChakraProvider } from '@chakra-ui/react'
+
+{/*Obtain user's movie list and each movie's Poster, Title, ~maybe Genre, and Average stars(~IMDB)*/}
 
 export default function Page() {
   return (
@@ -60,9 +37,9 @@ export default function Page() {
             <div className="col-10">
               <Box w='100%' minH='1400px' borderWidth='5px' boxShadow='dark-lg' borderColor='#171923' borderRadius='lg' backgroundColor='#A0AEC0'>
                 <Center>
-                  <Input size="lg" variant="outline" margin="50px" placeholder="Search your movies"/>
+                  <Input size="lg" variant="outline" margin="50px" placeholder="Search your movies"/>{/*Search filter's user's movie list displayed*/}
                 </Center>
-                <Tabs isFitted variant='solid-rounded' colorScheme='blue' margin='2px'>
+                <Tabs isFitted variant='solid-rounded' colorScheme='blue' margin='2px'>{/*~maybe remove these tabs*/}
                   <TabList mb='1em'>
                     <Tab>All</Tab>
                     <Tab>Comedy</Tab>
@@ -79,52 +56,20 @@ export default function Page() {
                       </div>
                       <Box borderWidth="2px" borderColor="grey" borderRadius="lg" minHeight="850px">
                         <Grid templateColumns='repeat(6, 1fr)' gap='10px' margin='15px'>
-                          <Image width='200px' height='250px' objectFit='cover' src="https://miro.medium.com/v2/resize:fit:720/format:webp/1*Etx-JkqJ5fGz9DnwLVlzNA.jpeg"></Image>
+{/*Loop for each movie added to the user's movie list*/}
+{/*Obtain each movie's Poster, Title, ~maybe Genre, and Average star rating*/}
+                          <Image width='200px' height='250px' objectFit='cover' src=""></Image>{/*Movie Poster URL*/}
                           <div>
-                            <Text fontSize={25} >V For Vendetta</Text>
-                            <Text> - Action, Horror, Comedy</Text>
-                            <StarRating />
-                            <Button colorScheme='blue' margin='5px' pos='absolute' > <Link href="review-my-movie">Reviews</Link></Button>
+                            <Text fontSize={25} >Movie Title</Text>{/*Movie title*/}
+                            <Text> - Genre, Genre, Genre</Text>{/*Movie Genres*/}
+                            <StarRating />{/*Movie's Total average Star rating -- OR -- IMDB average rating if we have that*/}
+                            <Button colorScheme='blue' margin='5px' pos='absolute' > <Link href="review-my-movie">Reviews</Link></Button>{/*Goes to reviews page of the selected movie*/}
                           </div>
-                          <Image width='200px' height='250px' objectFit='cover' src="https://musicimage.xboxlive.com/catalog/video.movie.8D6KGWZL59DS/image?locale=en-ie&mode=crop&purposes=BoxArt&q=90&h=300&w=200&format=jpg"></Image>
-                          <div>
-                            <Text fontSize={25} >The Green Mile</Text>
-                            <Text> - Action, Horror, Comedy</Text>
-                            <StarRating />
-                            <Button colorScheme='blue' margin='5px' pos='absolute'> <Link href="review-my-movie">Reviews</Link></Button>
-                          </div>
-                          <Image width='200px' height='250px' objectFit='cover' src="https://i.ebayimg.com/images/g/uHoAAOSw5yxjhNAI/s-l500.jpg"></Image>
-                          <div>
-                            <Text fontSize={25} >Contact</Text>
-                            <Text> - Action, Horror, Comedy</Text>
-                            <StarRating />
-                            <Button colorScheme='blue' margin='5px' pos='absolute'> <Link href="review-my-movie">Reviews</Link></Button>
-                          </div>
-                          <Image width='200px' height='250px' objectFit='cover' src="https://m.media-amazon.com/images/M/MV5BNGYyZGM5MGMtYTY2Ni00M2Y1LWIzNjQtYWUzM2VlNGVhMDNhXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_FMjpg_UX1000_.jpg"></Image>
-                          <div>
-                            <Text fontSize={25} >Pirates of The Caribbean: The Curse of The Black Pearl</Text>
-                            <Text> - Action, Horror, Comedy</Text>
-                            <StarRating />
-                            <Button colorScheme='blue' margin='5px' pos='absolute'> <Link href="review-my-movie">Reviews</Link></Button>
-                          </div>
-                          <Image width='200px' height='250px' objectFit='cover' src="https://images.squarespace-cdn.com/content/v1/5435f72ae4b09dbd92eb43ad/1585767951967-KP1BCETVEEQJ6WD7QOLQ/Hobbit+movie.jpg"></Image>
-                          <div>
-                            <Text fontSize={25} >The Hobbit: An Unexpected Journey</Text>
-                            <Text> - Action, Horror, Comedy</Text>
-                            <StarRating />
-                            <Button colorScheme='blue' margin='5px' pos='absolute'> <Link href="review-my-movie">Reviews</Link></Button>
-                          </div>
-                          <Image width='200px' height='250px' objectFit='cover' src="https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/p629_p_v8_af.jpg"></Image>
-                          <div>
-                            <Text fontSize={25} >Monty Python and The Holy Grail</Text>
-                            <Text> - Action, Horror, Comedy</Text>
-                            <StarRating />
-                            <Button colorScheme='blue' margin='5px' pos='absolute'> <Link href="review-my-movie">Reviews</Link></Button>
-                          </div>
+{/*End loop*/}
                         </Grid>
                       </Box>
                     </TabPanel>
-                    <TabPanel>
+                    <TabPanel>{/*Each tab panel would display the movies from the user's list but filter for only each repective genre*/}
 
                     </TabPanel>
                     <TabPanel>
@@ -138,9 +83,6 @@ export default function Page() {
                     </TabPanel>
                   </TabPanels>
                 </Tabs>
-                
-                
-                
               </Box>
             </div>
               <div className="col-1">
