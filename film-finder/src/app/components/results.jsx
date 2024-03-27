bcb29f53475b80807559f2784af8db4b29ea34e9
Alex Kuntz <alexanderaak@hotmail.com>
2024-03-26 22:59:00 -0600

    Modularized the Results Div

---
+++ film-finder/src/app/components/results.jsx
@@ -0,0 +1,34 @@
+import TheReturnOfTheKing from "./movie-picture.jsx";
+import StarRatingStatic from "./star_static.js";
+import { Button } from "@chakra-ui/react";
+
+export default function Results() {
+  return (
+    <div className="row">
+        <div className="col-3">
+            <TheReturnOfTheKing className="align-left" />
+            <Button marginTop={5} borderRadius={50}>
+                <a href='/review-my-movie'>+ Review Movie</a>
+            </Button>
+        </div>
+
+        <div className="col-9 fs-3">
+            <p className="h1">Generic movie</p>
+
+            <p className="h4 mt-5">
+                This is a plot summary This is a plot summary This is
+                a plot summary This is a plot summary This is a plot
+                summary This is a plot summary This is a plot summary
+                This is a plot summary This is a plot summary This is
+                a plot summary This is a plot summary This is a plot
+                summary This is a plot summary This is a plot summary
+            </p>
+
+            <div className="h3 mt-5">
+                <span> Average Review: <StarRatingStatic ratingNum={4}/> </span>
+            </div>
+        </div>
+    </div>
+   
+  );
+}
