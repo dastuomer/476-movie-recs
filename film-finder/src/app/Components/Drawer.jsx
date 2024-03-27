c6804e3626a5d3f7e29e2cfa2afd8e8f8d4da0e7
xander-vendramin <158534289+xander-vendramin@users.noreply.github.com>
2024-03-26 22:46:20 -0600

    Quick Burger Updates

--- film-finder/src/app/Components/Drawer.jsx
+++ film-finder/src/app/Components/Drawer.jsx
@@ -97,40 +97,21 @@ const NavDrawer = () =>  {
         <DrawerContent>
           <DrawerBody>
             <DrawerCloseButton />
-            {!session?(<DrawerHeader>Not Logged In</DrawerHeader>):(<DrawerHeader>Test</DrawerHeader>)}
+            {!session?(<DrawerHeader>Not Logged In</DrawerHeader>):(<DrawerHeader>Logged In As</DrawerHeader>)}
             <HStack margin={3}>
             {!session?(<Avatar size="lg"><AvatarBadge boxSize="1.25em" bg="green.500" /></Avatar>):(<Avatar size="lg"><AvatarBadge boxSize="1.25em" bg="green.500" /></Avatar>)}
               <Stack>
               {!session?(<Text fontSize="2xl" align={"center"}><Link href="profile">Guest</Link></Text>):(<Text fontSize="md" align={"center"}><Link href="profile">{session.user.email}</Link></Text>)}
               </Stack>
             </HStack>
-            <Input ref={friendLink} id="username" placeholder="+ Add friends" />
-            {/* We need to make it so when a link is entered a new freind is populated in the friends list */}
-            <Box marginTop={8} marginLeft={1} marginRight={1}>
-              <Accordion allowToggle>
-                <AccordionItem>
-                  <h2>
-                    <AccordionButton>
-                      <Box as="span" flex="1" textAlign="left" fontSize={"xl"}>
-                        Friends
-                      </Box>
-                      <AccordionIcon />
-                    </AccordionButton>
-                  </h2>
-                  <AccordionPanel pb={4}>
-                    <SearchBar placeholder="Search" dataSet={Users} value='first_name' />
-                  </AccordionPanel>
-                </AccordionItem>
-              </Accordion>
+            <Box marginTop={3} marginLeft={3} marginRight={3}>
+            {!session?(<Text fontFamily={"sans-serif"} fontSize="xl"></Text>):(<Text fontFamily={"sans-serif"} fontSize="xl"><Link href="view-my-movies">My movies</Link></Text>)}
             </Box>
             <Box marginTop={3} marginLeft={3} marginRight={3}>
-            {!session?(<Text fontFamily={"sans-serif"} fontSize="xl">Not Logged In</Text>):(<Text fontFamily={"sans-serif"} fontSize="xl"><Link href="view-my-movies">My movies</Link></Text>)}
+            {!session?(<Text fontFamily={"sans-serif"} fontSize="xl"></Text>):(<Text fontFamily={"sans-serif"} fontSize="xl"><Link href="blender">Blends</Link></Text>)}
             </Box>
             <Box marginTop={3} marginLeft={3} marginRight={3}>
-            {!session?(<Text fontFamily={"sans-serif"} fontSize="xl">Not Logged In</Text>):(<Text fontFamily={"sans-serif"} fontSize="xl"><Link href="blender">Blends</Link></Text>)}
-            </Box>
-            <Box marginTop={3} marginLeft={3} marginRight={3}>
-            {!session?(<Text fontFamily={"sans-serif"} fontSize="xl">Not Logged In</Text>):(<Text fontFamily={"sans-serif"} fontSize="xl"><Link href="view-friends">View Friends</Link></Text>)}
+            {!session?(<Text fontFamily={"sans-serif"} fontSize="xl"></Text>):(<Text fontFamily={"sans-serif"} fontSize="xl"><Link href="view-friends">View Friends</Link></Text>)}
             </Box>
           </DrawerBody>
           <DrawerFooter>
