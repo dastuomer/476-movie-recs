b0dd81dff992fc050bda5481aed5c203e7073408
xander-vendramin <158534289+xander-vendramin@users.noreply.github.com>
2025-04-08 16:18:08 -0600

    Initial Commit

--- film-finder/src/app/Components/logo.js
+++ film-finder/src/app/Components/logo.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 export default function Logo() {
   return (
     <div>
-      <Image src="/Film_Finder.png" width={100} height={100} alt="Brand Logo" />
+      <Image src="/Film_Finder.jpg" width={100} height={100} alt="Brand Logo" />
     </div>
   );
 }
